9c15f4ddfff6c1a3c922a6ded6b7baa08ba4893a
nagesh <designer.nagesh@gmail.com>
2020-02-08 15:08:28 +0530

    Updates

---
+++ precache-manifest.aae2322cb36cc37b3e32c7666bc4b83c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f831d7037e88fa8b0c68206f54fc241",
+    "url": "/myproject/index.html"
+  },
+  {
+    "revision": "ae669b720983e04bc9e1",
+    "url": "/myproject/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "fa72aa483c2231dcf9d1",
+    "url": "/myproject/static/js/2.23fc7b2a.chunk.js"
+  },
+  {
+    "revision": "ae669b720983e04bc9e1",
+    "url": "/myproject/static/js/main.f76b7961.chunk.js"
+  },
+  {
+    "revision": "53ed613d605950843be3",
+    "url": "/myproject/static/js/runtime-main.36d2610d.js"
+  }
+]);
